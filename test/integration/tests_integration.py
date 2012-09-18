eee452e10b4341a233b28c70625246deaa07acb3
Miguel Angel Garcia <miguelangel.garcia@gmail.com>
2012-09-18 19:17:27 +0200

    green. Added another test

--- test/integration/tests_integration.py
+++ test/integration/tests_integration.py
@@ -16,10 +16,18 @@ class BasicUsageTest(unittest.TestCase):
     def test_setting_the_title(self):
         given = """---
 title: this is the title
----
 """
+        expected = "<!doctype html><html><head><title>this is the title</title></head>"
 
-        expected = """<!doctype html><html><head><title>this is the title</title></head>"""
         current = self.sut.process(given)
+
         self.assertStartsWith(expected, current)
 
+    def test_has_a_initial_page(self):
+        given = """---
+title: this is the title
+"""
+        expected = '<div id="impress"><div class="step"><h1>this is the title</h1></div></div>'
+        current = self.sut.process(given)
+
+        self.assertContains(expected, current)
--- yampress/yampress.py
+++ yampress/yampress.py
@@ -5,14 +5,23 @@ import yaml
 class Yampress(object):
     def process(self, iml):
         generator = yaml.load_all(iml)
-        head = self._process_head(generator.next())
-        body = self._process_body(generator)
-        return '<!doctype html><html><head>{head}</head><body>{body}</body></html>'.format(head=head, body=body)
+        content = {}
+        config = generator.next()
+        content['head'] = self._process_head(config)
+        content['title'] = self._process_title(config)
+        content['body'] = self._process_body(generator)
+        return '<!doctype html><html><head>{head}</head><body><div id="impress">{title}{body}</div></body></html>'.format(**content)
 
-    def _process_head(self, data):
+    def _process_head(self, config):
         result = ''
-        if data.has_key('title'):
-            result += '<title>{}</title>'.format(data['title'])
+        if config.has_key('title'):
+            result += '<title>{}</title>'.format(config['title'])
+        return result
+
+    def _process_title(self, config):
+        result = ''
+        if config.has_key('title'):
+            result += '<div class="step"><h1>{}</h1></div>'.format(config['title'])
         return result
 
     def _process_body(self, data):
